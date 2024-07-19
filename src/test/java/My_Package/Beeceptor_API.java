bd67a052718579e78af2b271148343bb36ed4735
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-19 21:37:31 +0530

    Retesting done

--- src/test/java/My_Package/Beeceptor_API.java
+++ src/test/java/My_Package/Beeceptor_API.java
@@ -9,6 +9,7 @@ public class Beeceptor_API {
     
     final String Baseurl = "https://dummy-json.mock.beeceptor.com";
     
+    // Test to retrieve all employee data and verify the title of the first post
     @Test
     public void get_all_emp_data() {
         given().get(Baseurl + "/posts")
@@ -19,6 +20,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to retrieve a specific post by ID and verify its body content
     @Test
     public void Retrieve_post_by_ID() {
         given().get(Baseurl + "/posts/1")
@@ -29,6 +31,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to list all blog comments and verify the email of the fourth comment
     @Test
     public void Listing_all_blog_comments() {
         given().get(Baseurl + "/comments")
@@ -39,6 +42,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to retrieve a specific comment by ID and verify its email
     @Test
     public void Retrieve_comment() {
         given().get(Baseurl + "/comments/1")
@@ -49,6 +53,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to get a list of companies and verify the CEO name of the second company
     @Test
     public void Get_list_companies() {
         given().get(Baseurl + "/companies")
@@ -59,6 +64,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to retrieve details of a specific company by ID and verify its industry
     @Test
     public void Retrieve_details_company_ID() {
         given().get(Baseurl + "/companies/1")
@@ -69,6 +75,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to get a list of users and verify the name of the third user
     @Test
     public void Get_list_users() {
         given().get(Baseurl + "/users")
@@ -79,6 +86,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to retrieve details of a specific user by ID and verify their name
     @Test
     public void Retrieve_user_details() {
         given().get(Baseurl + "/users/1")
@@ -89,6 +97,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to get a list of standard roles and verify the name of the first role
     @Test
     public void Get_list_standard_roles() {
         given().get(Baseurl + "/roles")
@@ -99,6 +108,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to list popular quotes and verify the first quote
     @Test
     public void List_popular_quote() {
         given().get(Baseurl + "/quotes")
@@ -109,6 +119,7 @@ public class Beeceptor_API {
                 .log().all();
     }
     
+    // Test to list countries (assuming to verify the structure of the response)
     @Test
     public void List_countries() {
         given().get(Baseurl + "/countries")
