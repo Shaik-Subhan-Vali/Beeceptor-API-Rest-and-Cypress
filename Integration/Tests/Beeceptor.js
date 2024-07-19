712fff23497ca025386c5a1a54ac06c237194173
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-19 17:53:29 +0530

    Comments added for test/method

--- Integration/Tests/Beeceptor.js
+++ Integration/Tests/Beeceptor.js
@@ -1,42 +1,64 @@
 ///<reference types ="cypress"/>
 import RestAPI from "../../support/PageObjects/Beeceptor_objs";
+
 describe('Beeceptor API testing - POM', () => {
     
     let RAPI = new RestAPI();
-    let baseurl ="https://dummy-json.mock.beeceptor.com";
+    let baseurl = "https://dummy-json.mock.beeceptor.com";
 
-    it('List all available blog posts ',()=>{
-        RAPI.get_available_blog_posts("GET",baseurl,"/posts")
-    })
-    it('Retrieve a post by passing an integer ID',()=>{
-        RAPI.get_Retrieve_post("GET",baseurl,"/posts/1")
-    })
-    it('Listing all the blog comments',()=>{
-        RAPI.Listing_blog_comments("GET",baseurl,"/comments")
-    })
-    it('Retrieve a comment by passing and numeric/alphanumeric ID',()=>{
-        RAPI.Retrieve_comment("GET",baseurl,"/comments/1")
-    })
-    it('Get a list of all the companies',()=>{
-        RAPI.list_companies("GET",baseurl,"/companies")
-    })
-    it('Retrieve details about a company by passing company ID.',()=>{
-        RAPI.Retrieve_details_company("GET",baseurl,"/companies/1")
-    })
-    it('Get a list of all the users',()=>{
-        RAPI.Get_list_users("GET",baseurl,"/users")
-    })
-    it('Retrieve a users details',()=>{
-        RAPI.Get_list_users_deatils("GET",baseurl,"/users/1")
-    })
-    it('Get a list of standard roles.',()=>{
-        RAPI.Get_standard_roles("GET",baseurl,"/roles")
-    })
-    it('List of most pupular quotes',()=>{
-        RAPI.Get_popular_quotes("GET",baseurl,"/quotes")
-    })
-    it('List of all the countries in the world with their two-digit country code',()=>{
-        RAPI.List_all_countries("GET",baseurl,"/countries")
-    })
+    it('List all available blog posts', () => {
+        // Test to list all available blog posts
+        RAPI.get_available_blog_posts("GET", baseurl, "/posts");
+    });
 
-})
+    it('Retrieve a post by passing an integer ID', () => {
+        // Test to retrieve a post by passing an integer ID
+        RAPI.get_Retrieve_post("GET", baseurl, "/posts/1");
+    });
+
+    it('Listing all the blog comments', () => {
+        // Test to list all the blog comments
+        RAPI.Listing_blog_comments("GET", baseurl, "/comments");
+    });
+
+    it('Retrieve a comment by passing a numeric/alphanumeric ID', () => {
+        // Test to retrieve a comment by passing a numeric/alphanumeric ID
+        RAPI.Retrieve_comment("GET", baseurl, "/comments/1");
+    });
+
+    it('Get a list of all the companies', () => {
+        // Test to get a list of all the companies
+        RAPI.list_companies("GET", baseurl, "/companies");
+    });
+
+    it('Retrieve details about a company by passing company ID.', () => {
+        // Test to retrieve details about a company by passing company ID
+        RAPI.Retrieve_details_company("GET", baseurl, "/companies/1");
+    });
+
+    it('Get a list of all the users', () => {
+        // Test to get a list of all the users
+        RAPI.Get_list_users("GET", baseurl, "/users");
+    });
+
+    it('Retrieve a user\'s details', () => {
+        // Test to retrieve a user's details
+        RAPI.Get_list_users_deatils("GET", baseurl, "/users/1");
+    });
+
+    it('Get a list of standard roles.', () => {
+        // Test to get a list of standard roles
+        RAPI.Get_standard_roles("GET", baseurl, "/roles");
+    });
+
+    it('List of most popular quotes', () => {
+        // Test to list most popular quotes
+        RAPI.Get_popular_quotes("GET", baseurl, "/quotes");
+    });
+
+    it('List of all the countries in the world with their two-digit country code', () => {
+        // Test to list all countries with their country codes
+        RAPI.List_all_countries("GET", baseurl, "/countries");
+    });
+
+});
--- support/PageObjects/Beeceptor_objs.js
+++ support/PageObjects/Beeceptor_objs.js
@@ -1,165 +1,181 @@
-class RestAPI{
+class RestAPI {
 
-    get_available_blog_posts(method,burl,endpoints){
+    // Method to get available blog posts and assert on the title of the first post
+    get_available_blog_posts(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let title = obj[0].title;
-             expect(title).to.equal("Introduction to Artificial Intelligence");
-        })      
-    }
-    get_Retrieve_post(method,burl,endpoints){
-        cy.request({
-            method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
-            }
-        }).then((x)=>{
-            expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let body = obj.body;
-             expect(body).to.equal("Learn the basics of Artificial Intelligence and its applications in various industries.");
-            
-        })      
-    }
-    Listing_blog_comments(method,burl,endpoints){
-        cy.request({
-            method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
-            }
-        }).then((x)=>{
-            expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let email = obj[3].email;
-             expect(email).to.equal("sophia.lee@example.com");
-        })      
-    }
-    Retrieve_comment(method,burl,endpoints){
-        cy.request({
-            method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
-            }
-        }).then((x)=>{
-            expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let email = obj.email;
-             expect(email).to.equal("john.smith@example.com");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let title = obj[0].title;
+            expect(title).to.equal("Introduction to Artificial Intelligence");
+        });
     }
 
-    list_companies(method,burl,endpoints){
+    // Method to retrieve a post by passing an integer ID and assert on the body content
+    get_Retrieve_post(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let ceoName = obj[1].ceoName;
-             expect(ceoName).to.equal("Jane Doe");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let body = obj.body;
+            expect(body).to.equal("Learn the basics of Artificial Intelligence and its applications in various industries.");
+        });
     }
-    Retrieve_details_company(method,burl,endpoints){
+
+    // Method to list all blog comments and assert on the email of a specific comment
+    Listing_blog_comments(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let industry = obj.industry;
-             expect(industry).to.equal("Technology");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let email = obj[3].email;
+            expect(email).to.equal("sophia.lee@example.com");
+        });
     }
-    
-    Get_list_users(method,burl,endpoints){
+
+    // Method to retrieve a comment by passing a numeric/alphanumeric ID and assert on the email
+    Retrieve_comment(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let name = obj[2].name;
-             expect(name).to.equal("John Smith");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let email = obj.email;
+            expect(email).to.equal("john.smith@example.com");
+        });
     }
-    
-    Get_list_users_deatils(method,burl,endpoints){
+
+    // Method to list all companies and assert on the CEO name of the second company
+    list_companies(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let name = obj.name;
-             expect(name).to.equal("Emily Johnson");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let ceoName = obj[1].ceoName;
+            expect(ceoName).to.equal("Jane Doe");
+        });
     }
-    Get_standard_roles(method,burl,endpoints){
+
+    // Method to retrieve details about a company by passing company ID and assert on the industry
+    Retrieve_details_company(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let name = obj[0].name;
-             expect(name).to.equal("Admin");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let industry = obj.industry;
+            expect(industry).to.equal("Technology");
+        });
     }
-    
-    Get_popular_quotes(method,burl,endpoints){
+
+    // Method to get a list of all users and assert on the name of the third user
+    Get_list_users(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let quote = obj[0].quote;
-             expect(quote).to.equal("The only way to do great work is to love what you do.");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let name = obj[2].name;
+            expect(name).to.equal("John Smith");
+        });
     }
-    List_all_countries(method,burl,endpoints){
+
+    // Method to retrieve a user's details and assert on the name
+    Get_list_users_deatils(method, burl, endpoints) {
         cy.request({
             method: method,
-            url : burl + endpoints,
-            headers:{
-                "content-type":"application/json"
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
             }
-        }).then((x)=>{
+        }).then((x) => {
             expect(x.status).to.equal(200);
-             let obj = JSON.parse(JSON.stringify(x.body))
-             let code = obj[0];
-             expect(code).to.equal("[");
-        })      
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let name = obj.name;
+            expect(name).to.equal("Emily Johnson");
+        });
     }
-    
+
+    // Method to get a list of standard roles and assert on the name of the first role
+    Get_standard_roles(method, burl, endpoints) {
+        cy.request({
+            method: method,
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then((x) => {
+            expect(x.status).to.equal(200);
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let name = obj[0].name;
+            expect(name).to.equal("Admin");
+        });
+    }
+
+    // Method to get popular quotes and assert on the content of the first quote
+    Get_popular_quotes(method, burl, endpoints) {
+        cy.request({
+            method: method,
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then((x) => {
+            expect(x.status).to.equal(200);
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let quote = obj[0].quote;
+            expect(quote).to.equal("The only way to do great work is to love what you do.");
+        });
+    }
+
+    // Method to list all countries and assert on the first country code
+    List_all_countries(method, burl, endpoints) {
+        cy.request({
+            method: method,
+            url: burl + endpoints,
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then((x) => {
+            expect(x.status).to.equal(200);
+            let obj = JSON.parse(JSON.stringify(x.body))
+            let code = obj[0];
+            expect(code).to.equal("[");
+        });
+    }
+
 }
 
 export default RestAPI;
